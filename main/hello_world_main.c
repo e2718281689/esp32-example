79fa44cec3c789deb40e306936049258e23a9233
e2718281689 <2132906379@qq.com>
2022-02-08 13:56:49 +0800

    xxxd

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,25 +1,107 @@
-#include "sdkconfig.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_system.h"
+#include "esp_vfs_fat.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/spi_common.h"
+#include "sdmmc_cmd.h"
+#include "sdkconfig.h"
+//#include "Ap_29demo.h"
+#include "EPD.h"
 
-void myTask(void *pvParam)
+#define PIN_NUM_MISO 2
+#define PIN_NUM_MOSI 7
+#define PIN_NUM_CLK 6
+#define PIN_NUM_CS 1
+
+#define SD_MOUNT_POINT "/sdcard"
+
+sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+static const char *TAG = "pdm_rec_example";
+
+sdmmc_card_t *card;
+
+uint8_t gImage[2756] = {0};
+
+void mount_sdcard(void)
 {
-    while (1)
+    esp_err_t ret;
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = true,
+        .max_files = 5,
+        .allocation_unit_size = 8 * 1024};
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = PIN_NUM_MOSI,
+        .miso_io_num = PIN_NUM_MISO,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+    ret = spi_bus_initialize(host.slot, &bus_cfg, SPI_DMA_CH_AUTO);
+    if (ret != ESP_OK)
     {
-        printf("xxxxx\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGE(TAG, "Failed to initialize bus.");
+        return;
     }
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = PIN_NUM_CS;
+    slot_config.host_id = host.slot;
+
+    ret = esp_vfs_fat_sdspi_mount(SD_MOUNT_POINT, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK)
+    {
+        if (ret == ESP_FAIL)
+        {
+            ESP_LOGE(TAG, "Failed to mount filesystem.");
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                          "Make sure SD card lines have pull-up resistors in place.",
+                     esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    FILE *f = fopen(SD_MOUNT_POINT "/hello.txt", "w");
+    if (f == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+    fputs(TAG, f);
+    fclose(f);
 }
+
 void app_main(void)
 {
-    TaskHandle_t pxTask = NULL;
-    UBaseType_t iPriority = 0;
-    xTaskCreate(myTask, "one", 2048, NULL, 1, &pxTask);
-    iPriority = uxTaskPriorityGet(pxTask);
-    printf("iPriority=%d\n", iPriority);
 
-    vTaskPrioritySet(pxTask, 5);
+    EPD_init();
+    PIC_display_Clean_White(gImage);
+    PIC_display_White(gImage);
 
-    iPriority = uxTaskPriorityGet(pxTask);
-    printf("iPriority=%d\n", iPriority);
+    
+    EPD_refresh();
+    EPD_sleep();
 }
